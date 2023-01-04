ac172185642b8c3f2354f5c7a95425322d2f610b
itc-s21012 <s21012@std.it-college.ac.jp>
2023-01-04 16:27:31 +0900

    binding実装

--- app/src/main/java/jp/ac/it_college/std/s21012/myrecovery/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s21012/myrecovery/MainActivity.kt
@@ -1,11 +1,17 @@
 package jp.ac.it_college.std.s21012.myrecovery
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import jp.ac.it_college.std.s21012.myrecovery.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
